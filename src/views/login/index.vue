9c40968f905a8b4806915d985a0efe653f2316d5
Luyio <897707689@qq.com>
2020-10-09 17:24:36 +0800

    增加首页特效

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login-container">
+    <canvas id="c" style="position: absolute;"></canvas>
     <el-form
       class="login-form"
       autocomplete="on"
@@ -75,6 +76,9 @@ export default {
       rotate_pass: '',
     };
   },
+  mounted() {
+    this.setCanvas()
+  },
   methods: {
     showPwd() {
       if (this.pwdType === 'password') {
@@ -109,7 +113,45 @@ export default {
     end_pass() {
       this.rotate_pass = '';
     },
-  },
+    setCanvas() {
+        var c = document.getElementById("c");  //拿到画布并赋值给c
+        var ctx = c.getContext("2d");     //创建画笔,2d的
+ 
+        c.height = window.innerHeight;  //获取屏幕分辨率:高
+        c.width = window.innerWidth;    //宽
+        var chinese = "欢迎来到宝可梦世界";
+        chinese = chinese.split("");   //split() 方法用于把一个字符串分割成字符串数组。
+ 
+        var font_size = 26;   //字体大小
+        var columns = Math.floor(c.width/font_size);  //屏幕大小除以字体大小=字体个数;向下取整
+        //an array of drops - one per column
+        var drops = [];  //创建一个数组
+        for(var x = 0; x < columns; x++)
+        drops[x] = 1; 
+        //drawing the characters
+        function draw()
+        {
+        ctx.fillStyle = "rgba(0, 0, 0, 0.1)";//rgba:基础三色加不透明度
+        ctx.fillRect(0, 0, c.width, c.height);
+ 
+        ctx.fillStyle = "#009999"; //green text
+        ctx.font = font_size + "px arial";
+        //looping over drops
+        for(var i = 0; i < drops.length; i++)
+        {
+        var text = chinese[Math.floor(Math.random()*chinese.length)];
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
+ 
+        if(drops[i]*font_size > c.height && Math.random() > 0.975) //越接近一表示一起下落的可能性越低
+ 
+                drops[i] = 0;
+                 
+                drops[i]++;
+        }
+        }
+        setInterval(draw, 80);//时间间隔
+      },
+    }
 };
 </script>
 
@@ -184,6 +226,9 @@ $deep: #e3350d;
     width: 520px;
     padding: 35px 35px 15px 35px;
     margin: 120px auto;
+    background-color: rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 9px 3px #8e421e;
+    border-radius: 10px;
   }
   .tips {
     font-size: 14px;
